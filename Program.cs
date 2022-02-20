1d968831f52382f37d4a73e8f4eba95e34f35daa
Dan Narsavage <dannarsavage@users.noreply.github.com>
2022-02-20 09:56:14 -0700

    DO NOT DEPLOY.  Added mappings for desired endpoints.  Endpoints fail to serialize DateOnly objects.  Must create custom serializer(s).

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using StuffMySonSaysApi.Contracts.Interfaces;
+using StuffMySonSaysApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IQuoteRepository, QuoteRepository>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/quotes", (IQuoteRepository quoteRepository) => quoteRepository.GetAll);
+
+app.MapGet("/quotes/{id}", (int id, IQuoteRepository quoteRepository) => quoteRepository.GetQuote(id));
 
 app.Run();
--- Repositories/QuoteRepository.cs
+++ Repositories/QuoteRepository.cs
@@ -14,13 +14,13 @@ namespace StuffMySonSaysApi.Repositories
         {
             _quotes = new List<IQuote>
             {
-                new Quote(1, new DateOnly(2016, 12, 3), "Beeyah!  (Attempt at 'Spaghetti!')"),
-                new Quote(3, new DateOnly(2017, 7, 13), "A peloton is my favorite kind of 'ton!"),
-                new Quote(2, new DateOnly(2017, 12, 3), "I don't like peas. Peas make me sad. Oh no!"),
-                new Quote(4, new DateOnly(2018, 6, 8), "I wonder how wet THIS water is!"),
-                new Quote(5, new DateOnly(2020, 12, 3), "I like soup"),
-                new Quote(6, new DateOnly(2021, 12, 3), "I like soup"),
-                new Quote(7, new DateOnly(2022, 12, 3), "I like soup")
+                new Quote(1,    new DateOnly(2016, 12, 3),  "Beeyah!  (Attempt at 'Spaghetti!')"),
+                new Quote(3,    new DateOnly(2017, 7, 13),  "A peloton is my favorite kind of 'ton!"),
+                new Quote(2,    new DateOnly(2017, 12, 3),  "I don't like peas. Peas make me sad. Oh no!"),
+                new Quote(4,    new DateOnly(2018, 6, 8),   "I wonder how wet THIS water is!"),
+                new Quote(5,    new DateOnly(2020, 12, 3),  "I like soup"),
+                new Quote(6,    new DateOnly(2021, 12, 3),  "I like soup"),
+                new Quote(7,    new DateOnly(2022, 12, 3),  "I like soup")
             };
         }
 
