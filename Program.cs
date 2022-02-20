06a7ca31b91f011de4d732a0dd601acb4e14a289
Dan Narsavage <dannarsavage@users.noreply.github.com>
2022-02-20 11:21:08 -0700

    Added DateOnly serialization support.  Wrapped API results in IResult.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,39 @@
+using Microsoft.AspNetCore.Http.Json;
 using StuffMySonSaysApi.Contracts.Interfaces;
 using StuffMySonSaysApi.Repositories;
+using StuffMySonSaysApi.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSingleton<IQuoteRepository, QuoteRepository>();
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSingleton<IQuoteRepository, QuoteRepository>();
+
+builder.Services.Configure <JsonOptions> (options =>
+         options.SerializerOptions.Converters.Add(new DateOnlyConverter()));
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/quotes", (IQuoteRepository quoteRepository) => quoteRepository.GetAll);
+app.MapGet("/quotes", (IQuoteRepository quoteRepository) => GetAllQuotes(quoteRepository));
 
-app.MapGet("/quotes/{id}", (int id, IQuoteRepository quoteRepository) => quoteRepository.GetQuote(id));
+app.MapGet("/quotes/{id}", (int id, IQuoteRepository quoteRepository) => GetQuote(id, quoteRepository));
+
+IResult GetAllQuotes(IQuoteRepository quoteRepository)
+{
+    return Results.Ok(quoteRepository.GetAll());
+}
+
+IResult GetQuote(int id, IQuoteRepository quoteRepository)
+{
+    var quote = quoteRepository.GetQuote(id);
+
+    return (quote != null) 
+        ? Results.Ok(quote)
+        : Results.NotFound(null);
+}
 
 app.Run();
+
+
--- Repositories/QuoteRepository.cs
+++ Repositories/QuoteRepository.cs
@@ -15,8 +15,8 @@ namespace StuffMySonSaysApi.Repositories
             _quotes = new List<IQuote>
             {
                 new Quote(1,    new DateOnly(2016, 12, 3),  "Beeyah!  (Attempt at 'Spaghetti!')"),
-                new Quote(3,    new DateOnly(2017, 7, 13),  "A peloton is my favorite kind of 'ton!"),
-                new Quote(2,    new DateOnly(2017, 12, 3),  "I don't like peas. Peas make me sad. Oh no!"),
+                new Quote(2,    new DateOnly(2017, 7, 13),  "A peloton is my favorite kind of 'ton!"),
+                new Quote(3,    new DateOnly(2017, 12, 3),  "I don't like peas. Peas make me sad. Oh no!"),
                 new Quote(4,    new DateOnly(2018, 6, 8),   "I wonder how wet THIS water is!"),
                 new Quote(5,    new DateOnly(2020, 12, 3),  "I like soup"),
                 new Quote(6,    new DateOnly(2021, 12, 3),  "I like soup"),
